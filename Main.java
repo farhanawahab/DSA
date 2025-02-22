c550aec1c308fcf6df212dca5c5a94ee06402af7
farhanawahab <farhanawahab15@gmail.com>
2025-02-22 19:47:54 +0530

    made changes

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter your Input:");
+        int num = scan.nextInt();
+        String name = scan.next();
+        String y = scan.next();
+
+        //print
+        System.out.println(num);
+        System.out.println(name);
+        System.out.println(y);
     }
 }
