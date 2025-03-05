4d5033bb84b9c301f0d28ffc4e2b81e1d311fe3a
farhanawahab <farhanawahab15@gmail.com>
2025-03-05 23:12:06 +0530

    uploaded files

--- CountDigits.java
+++ CountDigits.java
@@ -1,10 +1,11 @@
-public class ReverseNum {
+public class CountDigits {
     public static void main(String[] args) {
         int n= 543211;
+        int count = 0;
         while(n>0){
-            int ld = n%10;
-            System.out.println(ld);
+            count = count+1;
             n = n/10;
         }
+        System.out.println(count);
     }
 }
